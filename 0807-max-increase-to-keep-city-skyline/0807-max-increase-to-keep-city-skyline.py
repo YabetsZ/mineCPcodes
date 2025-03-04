5982e57b15da8c9c99302a6e40ee8eba5e97d0a7
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-03-04 19:23:51 +0300

    Time: 8 ms (36.53%), Space: 17.7 MB (99.28%) - LeetHub

--- 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.py
+++ 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.py
@@ -1,13 +1,12 @@
 class Solution:
     def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        horizontal_max = []
-        for arr in grid:
-            horizontal_max.append(max(arr))
+        horizontal_max = [0] * n
         vertical_max = [0] * n
         for j in range(n):
             for i in range(n):
                 vertical_max[j] = max(vertical_max[j], grid[i][j])
+                horizontal_max[i] = max(horizontal_max[i], grid[i][j])
         result = 0
         for i in range(n):
             for j in range(n):
