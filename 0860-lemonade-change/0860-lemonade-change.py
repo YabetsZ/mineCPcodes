9282b573e1781b8ec7bf12c8b06b8026065f0fa3
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-03-05 23:52:19 +0300

    Time: 7 ms (36.2%), Space: 21.9 MB (79.77%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -0,0 +1,21 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        changes = defaultdict(int)
+        for money in bills:
+            if money == 20:
+                if changes[10] >= 1 and changes[5] >= 1:
+                    changes[10] -= 1
+                    changes[5] -= 1
+                elif changes[5] >= 3:
+                    changes[5] -= 3
+                else:
+                    return False
+            elif money == 10:
+                if changes[5] >= 1:
+                    changes[5] -= 1
+                else:
+                    return False
+            changes[money] += 1
+        return True
+
+                
