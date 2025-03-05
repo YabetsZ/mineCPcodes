da36888282ebb0d2ddd5ed6e0f4f355eda07eef5
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-03-05 19:37:45 +0300

    Created 'Array Splitting' by A2SV Hub

---
+++ 05-Mar-2025/Array Splitting 293358.py
@@ -0,0 +1,10 @@
+# Problem: Array Splitting - https://codeforces.com/problemset/problem/1197/C
+
+n, part = [int(x) for x in input().split()]
+
+nums = [int(x) for x in input().split()]
+
+sorted_diff = sorted([nums[i] - nums[i-1] for i in range(1, len(nums))])
+# print(sorted_diff)
+k = len(sorted_diff)
+print(sum(sorted_diff[:k - part + 1]))
