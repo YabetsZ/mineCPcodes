c8bff5999b1eae26426378bce4bfdebd0bf0b5fc
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-03-16 23:38:14 +0300

    Created 'Pascal's Triangle II - LeetCode' by A2SV Hub

---
+++ 16-Mar-2025/Pascal's Triangle II - LeetCode 308656.py
@@ -0,0 +1,13 @@
+# Problem: Pascal's Triangle II - LeetCode - https://leetcode.com/problems/pascals-triangle-ii/
+
+class Solution:
+    def getRow(self, row: int) -> List[int]:
+        result = []
+        factorial = [1]
+        for i in range(1,34):
+            factorial.append(i* factorial[-1])
+        for i in range(row+1):
+            val = factorial[row]/(factorial[row-i]*factorial[i])
+            result.append(int(val))
+
+        return result
