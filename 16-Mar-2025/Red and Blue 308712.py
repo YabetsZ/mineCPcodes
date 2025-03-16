08ed510e8e7312e34c8ea1c2da873c3a39d6140e
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-03-17 00:58:45 +0300

    Created 'Red and Blue' by A2SV Hub

---
+++ 16-Mar-2025/Red and Blue 308712.py
@@ -0,0 +1,20 @@
+# Problem: Red and Blue - https://codeforces.com/contest/1469/problem/B
+
+t = int(input())
+
+for _ in range(t):
+    rn = int(input())
+    r = [int(x) for x in input().split()] 
+    bn = int(input())
+    b = [int(x) for x in input().split()]
+
+    maxr, sumr = 0, 0
+    for num in r:
+        sumr += num
+        maxr = max(maxr, sumr)
+    maxb, sumb = 0, 0
+    for num in b:
+        sumb += num
+        maxb = max(maxb, sumb)
+    
+    print(maxr + maxb)
