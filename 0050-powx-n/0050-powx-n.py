05e00d6caff215d03f95e08448807ea495d38ea8
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-03-16 23:22:42 +0300

    Time: 0 ms (100%), Space: 17.9 MB (50.33%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,12 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def exp(x, n):
+            if n == 0:
+                return 1.0
+            half = exp(x, n//2)
+            if n % 2:
+                return half * half * x
+            else:
+                return half * half
+        result = exp(x, abs(n))
+        return result if n >= 0 else 1/result
