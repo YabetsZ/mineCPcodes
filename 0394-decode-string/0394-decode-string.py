8f6ed9c297b46afbf36bcf71439a9dbf98304c3a
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-03-16 22:11:18 +0300

    Time: 0 ms (100%), Space: 17.7 MB (84.31%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,29 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        def decode(s):
+            bracket = []
+            result = string = num = ""
+            
+            i = 0
+            while i < len(s):
+                char = s[i]
+                if char.isdigit():
+                    num += char
+                elif char == "[":
+                    bracket.append(char)
+                    while bracket:
+                        i += 1
+                        if s[i] == "[":
+                            bracket.append(s[i])
+                        elif s[i] == "]":
+                            bracket.pop()
+                        if bracket:
+                            string += s[i]
+                        # print(string, "char", i)
+                    result += int(num) * decode(string)
+                    num = string = ""
+                else:
+                    result += char
+                i += 1
+            return result 
+        return decode(s)  
