fd1b5eed4851e842c53a456380c2d9ae566d3b5d
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-03-19 18:10:44 +0300

    Time: 144 ms (38.12%), Space: 59.7 MB (57.83%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,15 +1,15 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         sol = []
-        def tryAll(cur,arr, n, k):
+        def tryAll(cur,arr, n = n, k= k):
             if len(arr) == k:
                 sol.append(arr.copy())
                 return
             elif cur > n:
                 return
             arr.append(cur)
-            tryAll(cur+1, arr, n, k)
+            tryAll(cur+1, arr,)
             arr.pop()
-            tryAll(cur+1, arr, n, k)
-        tryAll(1, [], n, k)
+            tryAll(cur+1, arr)
+        tryAll(1, [])
         return sol
