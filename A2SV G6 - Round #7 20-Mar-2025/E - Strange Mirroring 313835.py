75c5e969d1dd9fdc53b98befcbe0ef8e7f0dd5c1
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-03-20 23:48:01 +0300

    Created 'E - Strange Mirroring' by A2SV Hub

---
+++ A2SV G6 - Round #7 20-Mar-2025/E - Strange Mirroring 313835.py
@@ -0,0 +1,31 @@
+# Problem: E - Strange Mirroring - https://codeforces.com/gym/596141/problem/E
+
+t = int(input())
+import math
+def find(l, q, count):
+    if q <= l:
+        return (q, count)
+ 
+    exp = math.ceil(math.log(q/l, 2) - 1)
+ 
+    return find(l, q-(l*2**exp), count+1)
+ 
+ 
+ 
+ 
+for _ in range(t):
+    s = input()
+    n = int(input()) # number of queries
+ 
+    queries = [int(x) for x in input().split()]
+ 
+    result = []
+    for q in queries:
+        idx, count = find(len(s), q, 0)
+        if count%2:
+            result.append(s[idx-1].swapcase())
+        else:
+            result.append(s[idx-1])
+    
+ 
+    print(*result)
