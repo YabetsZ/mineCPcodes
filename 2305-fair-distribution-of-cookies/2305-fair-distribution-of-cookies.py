78174b335cc97b3aabca84fde35c29d7f5ce81d0
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-03-20 15:48:34 +0300

    Time: 6922 ms (5.18%), Space: 17.6 MB (98.2%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -0,0 +1,16 @@
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        child = [0]*k
+        def backtrack(i, minOfMax): # i is the current cookies index
+            if i == len(cookies):
+                return min(minOfMax, max(child))
+            if max(child) >= minOfMax:
+                return minOfMax
+            for idx, cur in enumerate(child):
+                child[idx] += cookies[i]
+                minOfMax = backtrack(i+1, minOfMax)
+                child[idx] -= cookies[i]
+            return minOfMax
+        
+        return backtrack(0, float('inf'))
+
