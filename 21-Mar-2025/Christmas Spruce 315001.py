f821d7ae63182c4dfa64a681796f2a67ad80f4f2
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-03-21 19:49:15 +0300

    Created 'Christmas Spruce' by A2SV Hub

---
+++ 21-Mar-2025/Christmas Spruce 315001.py
@@ -0,0 +1,28 @@
+# Problem: Christmas Spruce - https://codeforces.com/contest/913/problem/B
+
+from collections import defaultdict
+
+
+n = int(input())
+T = defaultdict(int)
+for node in range(2, n+1):
+    p = int(input())
+    if p not in T:
+        T[p] = []
+    T[p].append(node)
+
+# print(T)
+
+def dfs(node):
+    # if not T[node]:
+    #     return 1
+    count = 0
+    for child in T[node]:
+        if not T[child]:
+            count += 1
+        else:
+            if not dfs(child):
+                return False
+    return count >= 3
+
+print("Yes" if dfs(1) else "No")
