e7aa4f863af0aedbf27d1465d714f5ba5bc03399
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-03-21 23:43:41 +0300

    Created 'Find the Punishment Number of an Integer' by A2SV Hub

---
+++ 21-Mar-2025/Find the Punishment Number of an Integer 315355.py
@@ -0,0 +1,24 @@
+# Problem: Find the Punishment Number of an Integer - https://leetcode.com/problems/find-the-punishment-number-of-an-integer/description/
+
+class Solution:
+    def punishmentNumber(self, n: int) -> int:
+        def check_for_i(i, sqri, summed):
+            if sqri == 0 and summed == i:
+                return True
+            elif sqri == 0:
+                return False
+            for p in range(len(str(sqri))):
+                chopped = sqri//10**p
+                summed +=  chopped
+                # if summed > i:
+                #     return False
+                if check_for_i(i, sqri%10**p, summed):
+                    return True
+                summed -= chopped
+            return False
+        result = 0
+        for i in range(1, n+1):
+            if check_for_i(i, i**2, 0):
+                # print(i**2)
+                result += i**2
+        return result
