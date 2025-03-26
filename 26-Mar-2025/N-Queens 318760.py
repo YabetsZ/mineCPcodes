92655cbf4cdc34c82c9a19b5fe2671aadb620303
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-03-26 17:20:25 +0300

    Created 'N-Queens' by A2SV Hub

---
+++ 26-Mar-2025/N-Queens 318760.py
@@ -0,0 +1,33 @@
+# Problem: N-Queens - https://leetcode.com/problems/n-queens/description/
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        columns = set()
+        main_diagonal = set()
+        secondary_diagonal = set()
+        result = []
+        def place(row, unique):
+            if row == n:
+                return
+            for col in range(n):
+                if (col not in columns and
+                    row - col not in main_diagonal and
+                    row + col not in secondary_diagonal):
+
+                    columns.add(col)
+                    main_diagonal.add(row-col)
+                    secondary_diagonal.add(row+col)
+
+                    rowString = "".join(["."*col, "Q", "."*(n-col-1)])
+                    unique.append(rowString)
+
+                    if row == n-1:
+                        result.append(unique[:])
+                    place(row+1, unique)
+                    unique.pop()
+
+                    columns.discard(col)
+                    main_diagonal.discard(row-col)
+                    secondary_diagonal.discard(row+col)
+        place(0, [])
+        return result
