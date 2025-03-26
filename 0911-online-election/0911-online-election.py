6a35b9b3321ff3a3d0a8f301e1eae074b1fe819e
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-03-26 19:32:30 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0911-online-election/0911-online-election.py
@@ -0,0 +1,37 @@
+class TopVotedCandidate:
+
+    def __init__(self, persons: List[int], times: List[int]):
+        self.persons = persons
+        self.times = times
+        self.winner = []
+        prev_win = self.persons[0]
+        win_freq = {}
+        for num in self.persons:
+            win_freq[num] = win_freq.get(num, 0) + 1
+            if win_freq[prev_win] <= win_freq[num]:
+                prev_win = num
+            self.winner.append(prev_win)
+        
+
+    def binarySearch(self, target: int) -> int:
+        left, right = 0, len(self.times)
+        while left < right:
+            mid = left + (right - left)//2 + 1
+
+            if self.times[mid] == target:
+                return mid
+            elif self.times[mid] > target:
+                right = mid - 1
+            else:
+                left = mid
+        return left
+
+    def q(self, t: int) -> int:
+        idx = self.binarySearch(t)
+        return self.winner[idx]
+        
+
+
+# Your TopVotedCandidate object will be instantiated and called as such:
+# obj = TopVotedCandidate(persons, times)
+# param_1 = obj.q(t)
