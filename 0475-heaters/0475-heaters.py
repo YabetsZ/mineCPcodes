9c77e7a2472e05b7a3ef72cc242ab6c4dbfa9c90
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-04-05 22:48:52 +0300

    Time: 357 ms (9.37%), Space: 20.4 MB (85.86%) - LeetHub

---
+++ 0475-heaters/0475-heaters.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        houses.sort()
+        heaters.sort()
+        def verify(radius):
+            i = 0
+            for heat in heaters:
+                while i < len(houses):
+                    if not(heat-radius <= houses[i] <= heat+radius):
+                        break
+                    i += 1
+                if i == len(houses):
+                    break
+            return i == len(houses)
+        left, right = 0, 10**9
+        best = -1
+        while left <= right:
+            mid = left + (right-left)//2
+            if verify(mid):
+                best = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        return best
+        
