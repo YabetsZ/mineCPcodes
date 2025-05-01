53f783c25404690080b32fa90489f6992f3d2f7f
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-05-01 23:10:13 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -0,0 +1,19 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        visitedEdges = set()
+        bestAnswer = {}
+        def dfs(u):
+            if u not in bestAnswer:
+                bestAnswer[u] = 0
+            for v, w in graph[u]:
+                if (u, v) not in visitedEdges:
+                    visitedEdges.add((u, v))
+                    bestAnswer[v] = min(bestAnswer[u] + w, bestAnswer.get(v, float("inf")))
+                    dfs(v)
+        
+        graph = [[] for _ in range(n+1)]
+        for u, v, w in times:
+            graph[u].append((v, w))
+        dfs(k)
+        return -1 if len(bestAnswer)!=n else max(bestAnswer.values())
+        
