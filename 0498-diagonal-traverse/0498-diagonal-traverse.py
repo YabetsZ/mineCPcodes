1f904b3002cc50fb1876c06c3f2720398effa1b9
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-06-23 23:27:39 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        n, m = len(mat), len(mat[0])
+        def inbound(i, j):
+            return 0 <= i < n and 0 <= j < m
+        def traverse(row, col):
+            temp = []
+            while inbound(row, col):
+                temp.append(mat[row][col])
+                row += 1
+                col -= 1
+            if self.reverse:
+                result.extend(temp[::-1])
+            else:
+                result.extend(temp)
+            self.reverse = not self.reverse
+        
+        result, self.reverse = [], True
+
+        for j in range(n):
+            traverse(0, j)
+            
+        for i in range(1, n):
+            traverse(i, m-1)
+
+        return result
