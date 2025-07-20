6dab6ef3ae0368920bb6252800f030752711ac67
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-07-21 02:00:03 +0300

    Time: 62 ms (85.19%), Space: 21.9 MB (73.22%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.py
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.py
@@ -1,16 +1,15 @@
 class Solution:
-    def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
-        sortedArrival = sorted(times, key=lambda x: x[0])
-        result = {}
-        heap = []
+    def smallestChair(self, times: List[List[int]], target: int) -> int:
+        sortedArrival = sorted(times)
+        heap, available = [], []
         for i in range(len(times)):
-            arr, des = sortedArrival[i]
-            if  heap and heap[0] <= arr:
-                while heap:
-                    heappop(heap)
-                    if not heap or heap[0] > arr:
-                        break
-            heappush(heap, des)    
-            result[(arr, des)] = len(heap)-1
-        arr, des = times[targetFriend]
-        return result[(arr, des)]
+            arrive, leave = sortedArrival[i]
+            while heap and heap[0][0] <= arrive:
+                pleave, pPlace = heappop(heap)
+                heappush(available, pPlace)
+
+            appropriatePlace = heappop(available) if available else len(heap)
+            if [arrive, leave] == times[target]:
+                return appropriatePlace
+
+            heappush(heap, (leave, appropriatePlace))
