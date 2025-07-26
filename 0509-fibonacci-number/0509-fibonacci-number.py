f31a0f9e7d07e05156971ac065d8c932aa13d400
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-07-26 05:17:09 +0300

    Time: 28 ms (97.1%), Space: 17.5 MB (89.75%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,10 +1,8 @@
 class Solution:
     def fib(self, n: int, lookUp=None) -> int:
-        lookUp = {} if lookUp is None else lookUp
+        lookUp = {0:0, 1:1} if lookUp is None else lookUp
         if n in lookUp:
             return lookUp[n]
-        if n < 2:
-            return n
         
         lookUp[n] = self.fib(n-1, lookUp) + self.fib(n-2, lookUp)
         return lookUp[n]
