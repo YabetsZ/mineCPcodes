a621de99be1426b89414dfd865585aede9a079da
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-07-26 07:56:28 +0300

    Time: 379 ms (10.47%), Space: 24.4 MB (20.95%) - LeetHub

--- 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.py
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.py
@@ -2,19 +2,19 @@ class Solution:
     def minPathCost(self, grid: List[List[int]], moveCost: List[List[int]]) -> int:
         n, m = len(grid), len(grid[0])
         
-        def dp(i, j, lookUp=None):
-            lookUp = {x: x for x in grid[n-1]} if lookUp is None else lookUp
+        def dp(i, j):
             val = grid[i][j]
             if val in lookUp:
                 return lookUp[val]
             lookUp[val] = float("inf")
             for k in range(m):
-                temp = val + moveCost[val][k] + dp(i+1, k, lookUp)
+                temp = val + moveCost[val][k] + dp(i+1, k)
                 lookUp[val] = min(lookUp[val], temp)
             
             return lookUp[val]
-            
+
         result = float("inf")
+        lookUp = {x: x for x in grid[n-1]}
         for j in range(m):
             result = min(result, dp(0, j))
         
