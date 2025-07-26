674751f12dc37a1d1ce92e3a4833c9c75dc25741
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-07-26 05:20:40 +0300

    Time: 39 ms (65.01%), Space: 17.7 MB (40.53%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -2,12 +2,11 @@ class Solution:
     def fib(self, n: int) -> int:
         if n < 2:
             return n
-            
-        tab = deque([0, 1])
+
+        tab = [0, 1]
         
         for _ in range(2, n+1):
-            cur = tab[0] + tab[-1]
-            tab.popleft()
-            tab.append(cur)
+            tab[0], tab[1] = tab[1], tab[0]
+            tab[1] = tab[0] + tab[-1]
 
         return tab[-1]
