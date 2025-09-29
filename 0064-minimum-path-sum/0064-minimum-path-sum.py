3bb0fb1a150b635dc566f3fa0004b7fdbe0e850f
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-09-29 10:23:58 +0300

    Time: 77 ms (5.05%), Space: 25.4 MB (11.16%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -7,8 +7,8 @@ class Solution:
             if memo is None:
                 memo = {(0, 0) : grid[0][0]}
             if (i, j) not in memo:
-                left = dp(i, j-1) if in_bound(i, j-1) else float("inf")
-                top = dp(i-1, j) if in_bound(i-1, j) else float("inf")
+                left = dp(i, j-1, memo) if in_bound(i, j-1) else float("inf")
+                top = dp(i-1, j, memo) if in_bound(i-1, j) else float("inf")
                 memo[(i, j)] = min(left, top) + grid[i][j]
             return memo[(i, j)]
         
