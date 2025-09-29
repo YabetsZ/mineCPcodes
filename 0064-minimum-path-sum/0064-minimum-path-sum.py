590ba2829f18094fe8a728dd2167c99a8a640fc6
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-09-29 10:26:50 +0300

    Time: 27 ms (27.74%), Space: 20.2 MB (38.67%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -2,8 +2,9 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         def in_bound(i, j):
             return 0 <= i < len(grid) and 0 <= j < len(grid[0])
-        
-        def dp(i, j, memo=None):
+        n, m = len(grid), len(grid[0])
+        # recursive version
+        def dp_recursive(i, j, memo=None):
             if memo is None:
                 memo = {(0, 0) : grid[0][0]}
             if (i, j) not in memo:
@@ -11,5 +12,14 @@ class Solution:
                 top = dp(i-1, j, memo) if in_bound(i-1, j) else float("inf")
                 memo[(i, j)] = min(left, top) + grid[i][j]
             return memo[(i, j)]
+        # return dp_recursive(len(grid)-1, len(grid[0])-1)
+        table = [[float("inf")]*m for _ in range(n)]
+        table[0][0] = grid[0][0]
+        for i in range(n):
+            for j in range(m):
+                if i + 1 < n:
+                    table[i+1][j] = min(table[i][j] + grid[i+1][j], table[i+1][j])
+                if j + 1 < m:
+                    table[i][j+1] = min(table[i][j] + grid[i][j+1], table[i][j+1])
         
-        return dp(len(grid)-1, len(grid[0])-1)
+        return table[-1][-1]
