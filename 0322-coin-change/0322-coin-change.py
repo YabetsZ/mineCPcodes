975ea5198f354e121d79b97340643654a023d545
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-10-01 00:08:59 +0300

    Time: 788 ms (44.89%), Space: 18.3 MB (57.09%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,19 +1,31 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        def dp(remainder, memo=None):
-            if memo is None:
-                memo = {0: 0}
-            elif remainder < 0:
-                return float("inf")
-            if remainder in memo:
-                return memo[remainder]
-            
-            result = float("inf")
+        # ITERATIVE APPROACH
+        dp = [float("inf")]*(amount + 1)
+        dp[0] = 0 # basecase
+        for remainder in range(1, amount+1):
             for coin in coins:
-                result = min(result, dp(remainder-coin, memo))
-            
-            memo[remainder] = result + 1
-            return memo[remainder] 
+                if remainder-coin < 0: continue
+                dp[remainder] = min(dp[remainder], dp[remainder-coin]+1)
         
-        result = dp(amount)
-        return result if result != float("inf") else -1
+        return dp[amount] if dp[amount] != float("inf") else -1
+
+
+        # RECURSIVE APPROACH (1093ms)
+        # def dp(remainder, memo=None):
+        #     if memo is None:
+        #         memo = {0: 0}
+        #     elif remainder < 0:
+        #         return float("inf")
+        #     if remainder in memo:
+        #         return memo[remainder]
+            
+        #     result = float("inf")
+        #     for coin in coins:
+        #         result = min(result, dp(remainder-coin, memo))
+            
+        #     memo[remainder] = result + 1
+        #     return memo[remainder] 
+        
+        # result = dp(amount)
+        # return result if result != float("inf") else -1
