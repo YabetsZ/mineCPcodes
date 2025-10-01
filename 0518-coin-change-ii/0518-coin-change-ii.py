37dbbc7e883b6eec56734f12b8ec610a2376e19d
Yabets Zekaryas <134321167+YabetsZ@users.noreply.github.com>
2025-10-01 17:18:18 +0300

    Time: 344 ms (41.04%), Space: 17.9 MB (78.55%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -5,14 +5,9 @@ class Solution:
         dp[0] = 1
         
         for i in range(c):
-            dp_next = [0]*(amount + 1)
             for j in range(amount+1):
                 cur_coin = coins[i]
                 if cur_coin + j <= amount:
                     dp[j+cur_coin] += dp[j]
-                if i + 1 < c:
-                    dp_next[j] += dp[j]
-            if i + 1 < c:
-                dp = dp_next
 
         return dp[-1]
